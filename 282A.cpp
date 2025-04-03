77e4b87b1ec2f895acc6b74564b3ad630567948c
Yash-hacker-tech <y.sanghi2007@gmail.com>
2025-04-03 21:47:08 +0530

    Add files via upload

---
+++ 282A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int n;
+    cin >> n;
+    int x = 0;
+    for (int i = 0; i < n; ++i) 
+    {
+        string statement;
+        cin >> statement;
+        if (statement.find("++") != string::npos)   // nops means not present 
+        {
+            x++;
+        }
+        else if (statement.find("--") != string::npos) 
+        {
+            x--;
+        }
+    }
+    cout << x << endl;
+    return 0;
+}
